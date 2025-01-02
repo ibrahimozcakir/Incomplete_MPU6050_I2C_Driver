42b1033de7511625e4f6227315d94fa4d6bf15e7
İbrahim <143605308+ibrahimozcakir@users.noreply.github.com>
2025-01-02 16:19:51 +0300

    Delete MPU6050Drivers/I2C_Reset/I2C_Reset.h

--- MPU6050Drivers/I2C_Reset/I2C_Reset.h
+++
@@ -1,8 +0,0 @@
-#ifndef I2C_RESET_I2C_RESET_H_
-#define I2C_RESET_I2C_RESET_H_
-
-#include "stm32f4xx_hal.h"
-
-void I2C_ResetI2C(I2C_HandleTypeDef* I2C_Handle);
-
-#endif /* I2C_RESET_I2C_RESET_H_ */
